a30c3da17ecb5a41702ea81222b29955b1d162d8
prasadj9 <prasad.jambhale@nimapinfotech.com>
2024-11-25 16:53:58 +0530

    layout added and changed routing implementation

--- src/App.js
+++ src/App.js
@@ -3,37 +3,60 @@ import ComponentA from "./components/ComponentA";
 import { lazy, Suspense } from "react";
 import ConditionalRender from "./components/ConditionalRender";
 import HandleEvents from "./components/HandleEvents";
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import CallExpandable from "./components/CallExpandable";
 import RandomColorGeneraor from "./components/RandomColorGeneraor";
 import RefExample from "./components/RefExample";
+import Layout from "./components/Layout";
+
 //Lazy loading of component
 const MyComponent = lazy(() => import("./components/MyComponent"));
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <ComponentA />,
+        },
+        {
+          path: "/switch",
+          element: <ConditionalRender />,
+        },
+        {
+          path: "/event",
+          element: <HandleEvents />,
+        },
+        {
+          path: "/expandabletext",
+          element: <CallExpandable />,
+        },
+        {
+          path: "/randomcolor",
+          element: <RandomColorGeneraor />,
+        },
+        {
+          path: "/useref",
+          element: <RefExample />,
+        },
+        {
+          path: "/lazyload",
+          element: (
+            <Suspense fallback={<div>My component is Loading...</div>}>
+              <MyComponent />
+            </Suspense>
+          ),
+        },
+      ],
+    },
+  ]);
+
   return (
     <div className="App">
-      <Navbar />
-      {/* <ComponentA/>
-      <ConditionalRender/>
-      <HandleEvents/> */}
       <div className="content">
-        <Routes>
-          <Route path="/" element={<ComponentA />} />
-          <Route path="/switch" Component={ConditionalRender} />
-          <Route path="/event" Component={HandleEvents} />
-          <Route path="/expandabletext" Component={CallExpandable} />
-          <Route path="/randomcolor" Component={RandomColorGeneraor} />
-          <Route path="/useref" Component={RefExample} />
-          <Route
-            path="/lazyload"
-            element={
-              <Suspense fallback={<div>My component is Loading...</div>}>
-                <MyComponent />
-              </Suspense>
-            }
-          />
-        </Routes>
+        <RouterProvider router={router}/>
       </div>
     </div>
   );
